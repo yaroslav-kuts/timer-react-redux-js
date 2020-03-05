bf133391220a0096becbaa17044966b410e65d0a
zzzonked <yaroslavkuts@gmail.com>
2020-03-05 16:46:47 +0200

    Updates

---
+++ precache-manifest.a04be783ec7f8986176bfbe410b10a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01af9c32aa2c196a894950c916ec0099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a7728d9ebf1b99bfffb",
+    "url": "/static/css/main.31c01399.chunk.css"
+  },
+  {
+    "revision": "2041b14df5d9cd132ac0",
+    "url": "/static/js/2.c6bc4921.chunk.js"
+  },
+  {
+    "revision": "6d3c4316041b5905aa31b3ff080c57e2",
+    "url": "/static/js/2.c6bc4921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7728d9ebf1b99bfffb",
+    "url": "/static/js/main.d40c8bf2.chunk.js"
+  },
+  {
+    "revision": "7b9ba73799b122926a7d",
+    "url": "/static/js/runtime-main.b8256d19.js"
+  }
+]);
